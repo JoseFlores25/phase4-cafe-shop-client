9084209ab757909bcb0fcfb3f9be3db92b0ad136
JoseFlores25 <74205482+JoseFlores25@users.noreply.github.com>
2023-01-21 12:50:18 -0500

    Add home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,57 @@
+import { Box, Button, FormControl, OutlinedInput } from "@mui/material";
+import axios from "axios";
+import { useState } from "react";
+
+const Home = ({ handleGetUserData, navigate }) => {
+  const [userInfo, setUserInfo] = useState({ username: "", password: "" });
+
+  const handleUserInfo = (e, type) => {
+    const temp = { ...userInfo };
+    temp[type] = e.target.value;
+    setUserInfo(temp);
+  };
+
+  const handleLogin = async () => {
+    try {
+      const { data } = await axios.post("http://localhost:8000/login", {
+        ...userInfo,
+      });
+
+      console.log("[debug]", data, "data");
+      localStorage.setItem("user_id", data.id);
+      handleGetUserData(data);
+      navigate("/menu");
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "white",
+        // "&:hover": {
+        //   backgroundColor: "primary.main",
+        //   opacity: [0.9, 0.8, 0.7],
+        // },
+      }}
+    >
+      <FormControl sx={{ width: "25ch" }}>
+        <OutlinedInput
+          placeholder="username"
+          onChange={(e) => handleUserInfo(e, "username")}
+        />
+        <OutlinedInput
+          placeholder="Password"
+          onChange={(e) => handleUserInfo(e, "password")}
+        />
+        <Button onClick={handleLogin}>Login</Button>
+        <Button onClick={() => navigate("/authentication")}>Sign-up</Button>
+      </FormControl>
+    </Box>
+  );
+};
+
+export default Home;
