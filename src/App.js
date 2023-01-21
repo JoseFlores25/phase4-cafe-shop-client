02ccba40238ef586e3cc9908531944b4a3ef853c
JoseFlores25 <74205482+JoseFlores25@users.noreply.github.com>
2023-01-21 12:47:56 -0500

    More config

---
+++ src/App.js
@@ -0,0 +1,133 @@
+
+import './App.css';
+
+import Home from './pages/Home';
+import { Routes, Route, useNavigate } from "react-router-dom"
+import { useEffect, useState } from 'react';
+import Nav from './pages/Nav';
+import About from './pages/About';
+import Coffees from './pages/Coffees';
+import Contact from './pages/Contact';
+import Footer from './pages/Footer';
+import Auth from './pages/Auth';
+import Order from './pages/Order';
+import axios from 'axios';
+import Toppings from './pages/Toppings';
+import Feedback from './pages/Feedback';
+
+
+function App() {
+  const navigate = useNavigate()
+  const [errors, setErrors] = useState(false)
+  const [coffees, setCoffees] = useState([])
+  const [currentUser, setCurrentUser] = useState({})
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  const checkUserSession = async () => {
+    try {
+      const user_id = localStorage.getItem('user_id');
+
+      if (!user_id) return
+      const { data } = await axios.get(`http://localhost:8000/me/?user_id=${user_id}`)
+      setIsAuthenticated(true);
+      setCurrentUser(data)
+      await getUserMenu(user_id)
+      navigate('/menu')
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
+  const getUserMenu = async (user_id) => {
+    try {
+      const { data } = await axios.get(`http://localhost:8000/user/coffees?user_id=${user_id}`)
+      setCoffees(data)
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
+  const handleAddCoffee = async (payload) => {
+    try {
+      const { data } = await axios.post('http://localhost:8000/user/coffees', { ...payload, user_id: currentUser?.id })
+      setCoffees(data)
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
+  const handleDeleteCoffee = async (id) => {
+    try {
+      const { data } = await axios.post('http://localhost:8000/user/coffees/remove', { id, user_id: currentUser?.id })
+      setCoffees(data)
+      console.log('[debug]', data, 'deleted data')
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
+  const handleAddTopping = async (payload) => {
+    try {
+      await axios.post('http://localhost:8000/topping', { ...payload })
+      await checkUserSession()
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
+  const handleAddFeedback = async (payload) => {
+    try {
+      await axios.post('http://localhost:8000/feedback', { ...payload, user_id: currentUser?.id })
+      await checkUserSession()
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
+  const handleEditProduct = async (product) => {
+    try {
+      const { data } = await axios.put('http://localhost:8000/user/coffees', { ...product, user_id: currentUser?.id })
+      setCoffees(data)
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+  useEffect(() => {
+    checkUserSession()
+  }, [])
+
+
+  const handleGetUserData = async (user) => {
+    setCurrentUser(user)
+    await getUserMenu(user.id)
+    setIsAuthenticated(true)
+  }
+
+  function logout() {
+    sessionStorage.clear()
+    setIsAuthenticated(false)
+    setCurrentUser({})
+    setCoffees([])
+  }
+
+  return (
+    <div className="App">
+      <Nav currentUser={currentUser} navigate={navigate} isAuthenticated={isAuthenticated} logout={logout} />
+      <Routes>
+        <Route exact path="/" element={<Home isAuthenticated={isAuthenticated} handleGetUserData={handleGetUserData} navigate={navigate} />} />
+        <Route exact path="/order" element={<Order navigate={navigate} handleAddCoffee={handleAddCoffee} handleEditProduct={handleEditProduct} currentUser={currentUser} />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/menu" element={<Coffees currentUser={currentUser} isAuthenticated={isAuthenticated} handleDeleteCoffee={handleDeleteCoffee} coffees={coffees} navigate={navigate} />} />
+        <Route exact path="/topping" element={<Toppings handleAddTopping={handleAddTopping} isAuthenticated={isAuthenticated} navigate={navigate} />} />
+        <Route exact path="/feedback" element={<Feedback handleAddFeedback={handleAddFeedback} isAuthenticated={isAuthenticated} navigate={navigate} />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/authentication" element={<Auth handleGetUserData={handleGetUserData} navigate={navigate} />} />
+      </Routes>
+      <Footer />
+    </div>
+
+
+  );
+}
+
+export default App;
