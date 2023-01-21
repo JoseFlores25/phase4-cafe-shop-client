2a1eab6cc312a8c3eb1e7e8fc7310aad95985635
JoseFlores25 <74205482+JoseFlores25@users.noreply.github.com>
2023-01-21 12:49:37 -0500

    Add feedback page

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,52 @@
+import {
+  Typography,
+  Box,
+  FormControl,
+  OutlinedInput,
+  Button,
+} from "@mui/material";
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+
+const Feedback = ({ handleAddFeedback }) => {
+  const [rating, setRating] = useState({ title: "", description: "" });
+  const { state } = useLocation();
+  const { item } = state || {};
+
+  const handleInput = (e, type) => {
+    const temp = { ...rating };
+    temp[type] = e.target.value;
+    console.log('[debug]', temp, 'temp')
+    setRating(temp);
+  };
+
+  const addFeedback = () => {
+    handleAddFeedback({ ...rating, coffee_id: item.id });
+  };
+
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "white",
+      }}
+    >
+      <FormControl sx={{ width: "25ch" }}>
+        <OutlinedInput
+          placeholder="title"
+          onChange={(e) => handleInput(e, "title")}
+        />
+        <OutlinedInput
+          placeholder="description"
+          onChange={(e) => handleInput(e, "description")}
+        />
+        <Button onClick={addFeedback}>
+          <Typography>Add feedback</Typography>
+        </Button>
+      </FormControl>
+    </Box>
+  );
+};
+
+export default Feedback;
